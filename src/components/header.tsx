5c370826de9c9696d14d38efb57946e782be7151
wtnb93 <26590545+wtnb93@users.noreply.github.com>
2020-06-13 15:40:37 +0900

    fix basic layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,11 @@
 import React from "react"
 import { PageProps } from "gatsby"
 
-import { Avatar, Typography, Input } from 'antd'
-import { UserOutlined } from '@ant-design/icons'
+import { Avatar, Divider, Typography, Input, Tooltip, Button, Row, Col } from 'antd'
+import { SearchOutlined, GithubOutlined, SettingOutlined, UserOutlined, PropertySafetyFilled } from '@ant-design/icons'
+
+import FlexBox from "./flex-Box"
+import './input.less'
 
 interface Props {
   title: string
@@ -11,15 +14,26 @@ interface Props {
 const { Title } = Typography
 
 const Header: React.FC<Props> = ({ title }) => (
-  <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-    <div style={{ display: 'flex' }}>
+  <FlexBox justify="space-between">
+    <FlexBox>
       <Avatar shape="square" size={32} icon={<UserOutlined />} />
-      <Title level={3}>{title}</Title>
-    </div>
-    <>
-      <Input placeholder="search" style={{ width: "200px" }} size="small" />
-    </>
-  </div>
+      <Title level={3} style={{ marginBottom: 0, marginLeft: '8px' }}>{title}</Title>
+    </FlexBox>
+    <FlexBox>
+      <Input
+        placeholder="search"
+        prefix={<SearchOutlined />}
+      />
+      <Divider type="vertical" />
+      <Tooltip title="Turn on Darkmode">
+        <Button shape="circle" icon={<SettingOutlined />} />
+      </Tooltip>
+      <Divider type="vertical" />
+      <Tooltip title="Jump github">
+        <Button shape="circle" icon={<GithubOutlined />} />
+      </Tooltip>
+    </FlexBox>
+  </FlexBox>
 )
 
 export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,9 @@
 import React from "react"
 import { PageProps } from "gatsby"
 
-import { List, Input, Layout, Menu, Breadcrumb, Button, Typography, Space, Avatar } from 'antd'
-import { GlobalOutlined, UserOutlined, LaptopOutlined, NotificationOutlined, StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons'
+import { List } from 'antd'
 
 import Header from "../components/header"
-
-const { Title, Paragraph, Text } = Typography;
-const { SubMenu } = Menu;
-const { Content, Footer, Sider } = Layout;
-
-const listData = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://ant.design',
-    title: `ant design part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
-const IconText = ({ icon, text }) => (
-  <Space>
-    {React.createElement(icon)}
-    {text}
-  </Space>
-);
 
 const data = [
   {
@@ -46,26 +21,22 @@ const data = [
 ];
 
 const IndexPage: React.FC<PageProps<{}>> = () => (
-  <Layout style={{ margin: '50px auto', maxWidth: '800px', padding: '16px'}}>
-    <Header title="Blog" />
-    <Layout style={{ marginTop: '50px' }}>
-      <Content>
-      <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={item => (
-          <List.Item>
-            <List.Item.Meta
-              // avatar={<Avatar icon={<GlobalOutlined />} />}
-              title={<a href="https://ant.design">{item.title}</a>}
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-            />
-          </List.Item>
-        )}
-      />
-      </Content>
-    </Layout>
-  </Layout>
+  <div style={{ maxWidth: '42em', margin: '50px auto' }}>
+    <Header title="'s Blog" />
+    <List
+      itemLayout="horizontal"
+      style={{ marginTop: '50px' }}
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <List.Item.Meta
+            title={<a href="https://ant.design">{item.title}</a>}
+            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+          />
+        </List.Item>
+      )}
+    />
+  </div>
 )
 
 export default IndexPage

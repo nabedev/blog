d414321bc6394cbc65fdd357ad86f0ca04fc631d
wtnb93 <26590545+wtnb93@users.noreply.github.com>
2021-11-01 13:46:13 +0900

    Change to use yaml for products page resources

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const IndexPage: React.FC<PageProps> = () => (
     <Text>I'm Yuki Watanabe, a web developer.</Text>
     <Text>This site built with Gatsby.js and deployed to Netlify.</Text>
 
-    <Flex gap="size-150" marginTop="size-250">
+    <Flex direction={{base: 'column', S: 'row'}} gap="size-150" marginTop="size-250" alignItems="center">
       <OutlinedLink to="/blog" icon={<Document aria-label="document" />} heading="Blog" text="個人ブログ" />
       <OutlinedLink to="/products" icon={<Hammer aria-label="box" />} heading="Products" text="趣味で作ったプロダクトの紹介" />
       <OutlinedLink to="https://github.com/wtnb93" icon={<SiGithub size={24}/>} heading="Github" text="Github" />

a736306dec2f39491ffb046d540b5ef5b788f238
wtnb93 <26590545+wtnb93@users.noreply.github.com>
2021-11-02 00:39:28 +0900

    Fix production build broken

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,6 +1,16 @@
 import React from "react"
 import { Link, graphql, PageProps, Page } from "gatsby"
-import { Header, Content, Heading, Text, View, Flex, Divider, Image, Link as SpectrumLink } from '@adobe/react-spectrum'
+import {
+  Header,
+  Content,
+  Heading,
+  Text,
+  View,
+  Flex,
+  Divider,
+  Image,
+  Link as SpectrumLink,
+} from "@adobe/react-spectrum"
 
 const Products: React.FC<PageProps> = ({ data }) => (
   <Flex direction="column" gap="size-2000">
@@ -10,10 +20,17 @@ const Products: React.FC<PageProps> = ({ data }) => (
           {node.name}
         </Heading>
         <Divider />
-        <Flex direction={{base: 'column', S: 'row'}} justifyContent="space-between" gap="size-150" marginTop="size-150">
+        <Flex
+          direction={{ base: "column", S: "row" }}
+          justifyContent="space-between"
+          gap="size-150"
+          marginTop="size-150"
+        >
           <Content flex={1}>
             <Text>{node.description}</Text>
-            <SpectrumLink><Link to={node.repository}>repo</Link></SpectrumLink>
+            <SpectrumLink>
+              <Link to={node.repository}>repo</Link>
+            </SpectrumLink>
           </Content>
           <View padding="size-150" backgroundColor="gray-200" flex={1}>
             <Image src={node.demo} alt="demo" />
@@ -27,15 +44,15 @@ const Products: React.FC<PageProps> = ({ data }) => (
 export default Products
 
 export const pageQuery = graphql`
-  query ProductQuery{
+  query ProductQuery {
     allProductsYaml {
-    nodes {
-      name
-      repository
-      description
-      id
-      demo
+      nodes {
+        name
+        repository
+        description
+        id
+        demo
+      }
     }
-  }
   }
 `
